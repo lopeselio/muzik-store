4ec3718e2e86b119635bd7345de5b5a3a06d502c
lopeselio <lopeselio@gmail.com>
2020-09-07 12:22:54 +0530

    custom button setup template

--- client/src/components/utils/button.js
+++ client/src/components/utils/button.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const MyButton = () => {
+const MyButton = (props) => {
   const buttons = () => {
     let template = ''
 
     switch (props.type) {
       case 'default':
+        template =
+          <Link
+            className='link_default'
+            to={props.linkTo}
+            {...props.addStyles}
+          >
+            {props.title}
+          </Link>
+        break
 
       default:
         template = ''
