9c3282e0f171eb079a5ecdbd007ec088ecb277cf
lopeselio <lopeselio@gmail.com>
2020-09-21 12:41:46 +0530

    actions to validate empty string, email input

---
+++ client/src/components/utils/Form/formActions.js
@@ -0,0 +1,39 @@
+// Form validation during the login
+
+export const validate = (element, formdata = []) => {
+  let error = [true, '']
+  if (element.validation.email) {
+    const valid = /\S+@\S+\.\S+/.test(element.value)
+    const message = `${!valid ? 'Must be a valid email' : ''}`
+    error = !valid ? [valid, message] : error
+  }
+  if (element.validation.required) {
+    const valid = element.value.trim() != ''
+    const message = `${!valid ? 'This field is required' : ''}`
+    error = !valid ? [valid, message] : error
+  }
+  return error
+}
+
+export const update = (element, formdata, formName) => {
+  const newFormdata = {
+    ...formdata
+  }
+
+  const newElement = {
+    ...newFormdata[element.id]
+  }
+
+  newElement.value = element.event.target.value
+
+  if (element.blur) {
+    const validData = validate(newElement, formdata)
+    newElement.valid = validData[0]
+    newElement.validationMessage = validData[1]
+  }
+
+  newElement.touched = element.blur
+  newFormdata[element.id] = newElement
+
+  return newFormdata
+}
