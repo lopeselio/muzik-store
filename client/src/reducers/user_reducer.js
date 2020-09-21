1b8bdb3fbb189af3cc891ba382761ceacbed8b45
lopeselio <lopeselio@gmail.com>
2020-09-21 16:08:34 +0530

    redux

--- client/src/reducers/user_reducer.js
+++ client/src/reducers/user_reducer.js
@@ -1,5 +1,11 @@
+import {
+  LOGIN_USER
+} from '../actions/types'
+
 export default function (state = {}, action) {
   switch (action.type) {
+    case LOGIN_USER:
+      return { ...state, loginSucces: action.payload }
     default:
       return state
   }
