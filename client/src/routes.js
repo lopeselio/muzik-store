cb311e59c31e8ae8769de2237919e259e729a8a4
lopeselio <lopeselio@gmail.com>
2020-09-21 16:31:57 +0530

    added register route

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,12 +4,15 @@ import { Switch, Route } from 'react-router-dom'
 import Layout from './hoc/layout'
 import Home from './components/Home'
 import RegisterLogin from './components/Register_login/index'
+import Register from './components/Register_login/register'
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
+        <Route path='/register' exact component={Register} />
         <Route path='/register_login' exact component={RegisterLogin} />
+        
         <Route path='/' exact component={Home} />
         
       </Switch>
