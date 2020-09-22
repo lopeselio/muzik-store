2a2ca6cd46b9f390bc9b18235e888f4fbda79c77
lopeselio <lopeselio@gmail.com>
2020-09-22 19:08:56 +0530

    product actions for fetch by arrival

--- client/src/actions/products_actions.js
+++ client/src/actions/products_actions.js
@@ -1,15 +1,28 @@
-import axios from 'axios';
+import axios from 'axios'
 import {
   GET_PRODUCTS_BY_SELL,
   GET_PRODUCTS_BY_ARRIVAL
-} from './types';
+} from './types'
 
 import { PRODUCT_SERVER } from '../components/utils/misc'
 
-export function getProductsBySell(){
-
+export function getProductsBySell () {
+  // ?sortBy=sold&order=desc&limit=100
+  const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=sold&order=desc&limit=4`)
+    .then(response => response.data)
+  return {
+    type: GET_PRODUCTS_BY_SELL,
+    payload: request
+  }
 }
 
-export function getProductsByArrival(){
+export function getProductsByArrival () {
+  // ?sortBy=sold&order=desc&limit=100
+  const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=createdAt&order=desc&limit=4`)
+    .then(response => response.data)
+  return {
+    type: GET_PRODUCTS_BY_ARRIVAL,
+    payload: request
+  }
 
 }
