733db532c33debdc02a9c09956db27db89108bf9
lopeselio <lopeselio@gmail.com>
2020-09-22 07:42:28 +0530

    reducer

--- client/src/reducers/user_reducer.js
+++ client/src/reducers/user_reducer.js
@@ -1,7 +1,7 @@
 import {
   LOGIN_USER,
   REGISTER_USER,
-  // AUTH_USER
+  AUTH_USER
 } from '../actions/types'
 
 export default function (state = {}, action) {
@@ -10,8 +10,8 @@ export default function (state = {}, action) {
       return { ...state, register: action.payload }
     case LOGIN_USER:
       return { ...state, loginSucces: action.payload }
-    // case AUTH_USER:
-    //   return { ...state, userData: action.payload }
+    case AUTH_USER:
+      return { ...state, userData: action.payload }
     default:
       return state
   }
