44725a249c79a081c13cb888f9115acac5116735
lopeselio <lopeselio@gmail.com>
2020-09-22 07:43:11 +0530

    authjs

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,21 +1,18 @@
-const { User } = require('./../models/user');
+const { User } = require('./../models/user')
+const auth = (req, res, next) => {
+  const token = req.cookies.w_auth
 
-let auth = (req,res,next) => {
-    let token = req.cookies.w_auth;
-
-    User.findByToken(token,(err,user)=>{
-        if(err) throw err;
-        if(!user) return res.json({
-            isAuth: false,
-            error: true
-        });
-
-        req.token = token;
-        req.user = user;
-        next();
+  User.findByToken(token, (err, user) => {
+    if (err) throw err
+    if (!user) return res.json({
+      isAuth: false,
+      error: true
     })
 
+    req.token = token
+    req.user = user
+    next()
+  })
 }
-
 
 module.exports = { auth }
