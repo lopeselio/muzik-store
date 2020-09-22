e82c7b70d004f7cf911bb73148be587f74c0df18
lopeselio <lopeselio@gmail.com>
2020-09-22 07:41:25 +0530

    routes auth

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Layout from './hoc/layout'
-// import Auth from './hoc/auth'
+import Auth from './hoc/auth'
 
 import Home from './components/Home'
 import RegisterLogin from './components/Register_login'
@@ -14,11 +14,11 @@ const Routes = () => {
   return (
     <Layout>
       <Switch>
-        <Route path='/user/dashboard' exact component={UserDashboard} />
+        <Route path='/user/dashboard' exact component={Auth(UserDashboard, true)} />
 
-        <Route path='/register' exact component={Register} />
-        <Route path='/register_login' exact component={RegisterLogin} />
-        <Route path='/' exact component={Home} />
+        <Route path='/register' exact component={Auth(Register, false)} />
+        <Route path='/register_login' exact component={Auth(RegisterLogin, false)} />
+        <Route path='/' exact component={Auth(Home, null)} />
       </Switch>
     </Layout>
 
