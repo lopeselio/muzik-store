ff18fe483518c84576fce9cc169e7af2a6b2cc01
lopeselio <lopeselio@gmail.com>
2020-09-22 15:57:20 +0530

    models

--- server/models/product.js
+++ server/models/product.js
@@ -1,58 +1,59 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const productSchema = mongoose.Schema({
-  name: {
-    required: true,
-    type: String,
-    unique: 1,
-    maxLength: 100
-  },
-  description: {
-    required: true,
-    type: String,
-    maxLength: 10000
-  },
-  price: {
-    required: true,
-    type: Number,
-    maxLength: 255
-  },
-  brand: {
-    type: Schema.Types.ObjectId,
-    ref: 'Brand',
-    required: true
-  },
-  shipping: {
-    required: true,
-    type: Boolean
-  },
-  available: {
-    required: true,
-    type: Boolean
-  },
-  wood: {
-    type: Schema.Types.ObjectId,
-    ref: 'Wood',
-    required: true
-  },
-  frets: {
-    required: true,
-    type: Number
-  },
-  sold: {
-    type: Number,
-    maxLength: 100,
-    default: 0
-  },
-  publish: {
-    type: Boolean,
-    required: true
-  },
-  images: {
-    type: Array,
-    default: []
-  }
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-}, { timestamps: true })
-const Product = mongoose.model('Product', productSchema)
+const productSchema = mongoose.Schema({
+    name:{
+        required: true,
+        type: String,
+        unique: 1,
+        maxlength:100
+    },
+    description:{
+        required: true,
+        type: String,
+        maxlength:100000
+    },
+    price:{
+        required: true,
+        type: Number,
+        maxlength: 255
+    },
+    brand:{
+        type: Schema.Types.ObjectId,
+        ref: 'Brand',
+        required: true
+    },
+    shipping:{
+        required: true,
+        type: Boolean
+    },
+    available:{
+        required: true,
+        type: Boolean
+    },
+    wood:{
+        type: Schema.Types.ObjectId,
+        ref: 'Wood',
+        required: true
+    },
+    frets:{
+        required: true,
+        type: Number
+    },
+    sold:{
+        type: Number,
+        maxlength: 100,
+        default: 0
+    },
+    publish:{
+        required: true,
+        type: Boolean
+    },
+    images:{
+        type: Array,
+        default:[]
+    }
+},{timestamps:true});
+
+const Product = mongoose.model('Product',productSchema);
 module.exports = { Product }
--- server/models/wood.js
+++ server/models/wood.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose')
-const woodSchema = mongoose.Schema({
-  name: {
-    required: true,
-    type: String,
-    unique: 1,
-    maxLength: 100
-  }
-})
+const mongoose = require('mongoose');
 
-const Wood = mongoose.model('Wood', woodSchema)
+const woodSchema = mongoose.Schema({
+    name:{
+        required: true,
+        type: String,
+        unique: 1,
+        maxlength: 100
+    }
+});
+
+const Wood = mongoose.model('Wood',woodSchema);
 
 module.exports = { Wood }
