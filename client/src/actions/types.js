2b0cd47f9a1b4a8195cbc7427edd8a980a68a1cd
lopeselio <lopeselio@gmail.com>
2020-09-23 17:24:34 +0530

    added action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,7 +1,9 @@
-export const LOGIN_USER = 'login_user';
-export const REGISTER_USER = 'register_user';
-export const AUTH_USER = 'auth_user';
-export const LOGOUT_USER = 'logout_user';
+export const LOGIN_USER = 'login_user'
+export const REGISTER_USER = 'register_user'
+export const AUTH_USER = 'auth_user'
+export const LOGOUT_USER = 'logout_user'
 
-export const GET_PRODUCTS_BY_SELL = 'get_products_by_sell';
-export const GET_PRODUCTS_BY_ARRIVAL = 'get_products_by_arrival';
+export const GET_PRODUCTS_BY_SELL = 'get_products_by_sell'
+export const GET_PRODUCTS_BY_ARRIVAL = 'get_products_by_arrival'
+export const GET_BRANDS = 'get_brands'
+export const GET_WOODS = 'get_woods'
