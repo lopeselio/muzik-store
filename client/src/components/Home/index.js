77e2797624c67c13b45f7bb8669b117a33a92c14
lopeselio <lopeselio@gmail.com>
2020-09-23 05:41:28 +0530

    home

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import HomeSlider from './home_slider'
 import HomePromotion from './home_promotion'
+import CardBlock from '../utils/card_block'
 
 import { connect } from 'react-redux'
 import { getProductsBySell, getProductsByArrival } from '../../actions/products_actions'
@@ -21,7 +22,9 @@ class Home extends Component {
     return (
       <div>
         <HomeSlider />
+        <CardBlock list={this.props.products.bySell} title='Best selling Guitars' />
         <HomePromotion />
+        <CardBlock list={this.props.products.byArrival} title='New Arrivals' />
       </div>
     )
   }
