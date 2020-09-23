eb741bfb3897af3fb2faefa95fdc80f06b7cb161
lopeselio <lopeselio@gmail.com>
2020-09-23 07:19:02 +0530

    button

--- client/src/components/utils/button.js
+++ client/src/components/utils/button.js
@@ -1,33 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
- 
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faShoppingBag from '@fortawesome/fontawesome-free-solid/faShoppingBag'
+
 const MyButton = (props) => {
+  const buttons = () => {
+    let template = ''
 
-    const buttons = () => {
-        let template = '';
-
-        switch(props.type){
-            case "default":
-                template = <Link
-                    className="link_default"
-                    to={props.linkTo}
-                    {...props.addStyles}
-                >
-                    {props.title}
-                </Link>
-            break;
-            default:
-                template='';
-        }
-        return template
+    switch (props.type) {
+      case 'default':
+        template = <Link
+          className={!props.altClass ? 'link_default' : props.altClass}
+          to={props.linkTo}
+          {...props.addStyles}
+        >
+          {props.title}
+        </Link>
+        break
+      case 'bag_link':
+        template =
+          <div
+            className='bag_link'
+            onClick={() => {
+              props.runAction()
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faShoppingBag}
+            />
+          </div>
+        break
+      default:
+        template = ''
     }
+    return template
+  }
 
+  return (
+    <div className='my_link'>
+      {buttons()}
+    </div>
+  )
+}
 
-    return (
-        <div className="my_link">
-             {buttons()}   
-        </div>
-    );
-};
-
-export default MyButton;
+export default MyButton
