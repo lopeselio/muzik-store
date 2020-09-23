a3c3d10eaf53b111acb56c86b7e52d8369489b03
lopeselio <lopeselio@gmail.com>
2020-09-23 17:24:56 +0530

    Shop screen

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -1,20 +1,112 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import PageTop from '../utils/page_top';
-import { connect } from 'react-redux'
+
+import { frets,price } from '../utils/Form/fixed_categories';
+
+import { connect } from 'react-redux';
+import { getBrands, getWoods } from '../../actions/products_actions';
+
+import CollapseCheckbox from '../utils/collapseCheckbox';
+import CollapseRadio from '../utils/collapseRadio';
 
 class Shop extends Component {
-  render() {
-    return (
-      <div>
-        shop
-      </div>
-    )
-  }
+
+    state = {
+        grid:'',
+        limit:6,
+        skip:0,
+        filters:{
+            brand:[],
+            frets:[],
+            wood:[],
+            price:[]
+        }
+    }
+
+    componentDidMount(){
+        this.props.dispatch(getBrands());
+        this.props.dispatch(getWoods());
+    }
+
+    handlePrice = (value) => {
+        const data = price;
+        let array = [];
+
+        for(let key in data){
+            if(data[key]._id === parseInt(value,10)){
+                array = data[key].array
+            }
+        }
+        return array;
+    }
+
+
+    handleFilters = (filters,category) => {
+       const newFilters = {...this.state.filters}
+       newFilters[category] = filters;
+
+        if(category === "price"){
+            let priceValues = this.handlePrice(filters);
+            newFilters[category] = priceValues
+        }
+
+       this.setState({
+           filters: newFilters
+       })
+    }
+
+    render() {
+        console.log(this.state.filters)
+        const products = this.props.products;
+        return (
+            <div>
+                <PageTop
+                    title="Browse Products"
+                />
+                <div className="container">
+                    <div className="shop_wrapper">
+                        <div className="left">
+                            <CollapseCheckbox
+                                initState={true}
+                                title="Brands"
+                                list={products.brands}
+                                handleFilters={(filters)=> this.handleFilters(filters,'brand')}
+                            />
+                             <CollapseCheckbox
+                                initState={false}
+                                title="Frets"
+                                list={frets}
+                                handleFilters={(filters)=> this.handleFilters(filters,'frets')}
+                            />
+                            <CollapseCheckbox
+                                initState={false}
+                                title="Wood"
+                                list={products.woods}
+                                handleFilters={(filters)=> this.handleFilters(filters,'wood')}
+                            />
+                             <CollapseRadio
+                                initState={true}
+                                title="Price"
+                                list={price}
+                                handleFilters={(filters)=> this.handleFilters(filters,'price')}
+                            />
+                           
+                        </div>
+                        <div className="right">
+                            right
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
+
 const mapStateToProps = (state) => {
-  return {
-    products: state.prop
-  }
+    return {
+        products: state.products
+    }
 }
-export default connect()(Shop)
+
+export default connect(mapStateToProps)(Shop);
