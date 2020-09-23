d201795ba88182d235f55f5f34e5b382827dfe3e
lopeselio <lopeselio@gmail.com>
2020-09-23 17:24:20 +0530

    added actions for products

--- client/src/actions/products_actions.js
+++ client/src/actions/products_actions.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 import {
   GET_PRODUCTS_BY_SELL,
-  GET_PRODUCTS_BY_ARRIVAL
+  GET_PRODUCTS_BY_ARRIVAL,
+  GET_BRANDS,
+  GET_WOODS
 } from './types'
 
 import { PRODUCT_SERVER } from '../components/utils/misc'
@@ -10,6 +12,7 @@ export function getProductsBySell () {
   // ?sortBy=sold&order=desc&limit=100
   const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=sold&order=desc&limit=4`)
     .then(response => response.data)
+
   return {
     type: GET_PRODUCTS_BY_SELL,
     payload: request
@@ -17,12 +20,35 @@ export function getProductsBySell () {
 }
 
 export function getProductsByArrival () {
-  // ?sortBy=sold&order=desc&limit=100
   const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=createdAt&order=desc&limit=4`)
     .then(response => response.data)
+
   return {
     type: GET_PRODUCTS_BY_ARRIVAL,
     payload: request
   }
+}
 
+/// /////////////////////////////////
+/// ///        CATEGORIES
+/// /////////////////////////////////
+
+export function getBrands () {
+  const request = axios.get(`${PRODUCT_SERVER}/brands`)
+    .then(response => response.data)
+
+  return {
+    type: GET_BRANDS,
+    payload: request
+  }
+}
+
+export function getWoods () {
+  const request = axios.get(`${PRODUCT_SERVER}/woods`)
+    .then(response => response.data)
+
+  return {
+    type: GET_WOODS,
+    payload: request
+  }
 }
