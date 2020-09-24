7c03ea9a79a8db20ea748245821f1157a4b58497
lopeselio <lopeselio@gmail.com>
2020-09-24 12:54:13 +0530

    added route for file upload

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,30 +1,32 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import Layout from './hoc/layout'
-import Auth from './hoc/auth'
+import Layout from './hoc/layout';
+import Auth from './hoc/auth';
 
-import Home from './components/Home'
-import RegisterLogin from './components/Register_login'
-import Register from './components/Register_login/register'
-import Shop from './components/Shop'
+import Home from './components/Home';
+import RegisterLogin from './components/Register_login';
+import Register from './components/Register_login/register';
+import Shop from './components/Shop';
 
-import UserDashboard from './components/User'
+import UserDashboard from './components/User';
+import AddProduct from './components/User/Admin/add_product';
 
 const Routes = () => {
-  return (
+  return(
     <Layout>
       <Switch>
-        <Route path='/user/dashboard' exact component={Auth(UserDashboard, true)} />
+        <Route path="/user/dashboard" exact component={Auth(UserDashboard,true)}/>
+        <Route path="/admin/add_product" exact component={Auth(AddProduct,true)}/>
 
-        <Route path='/register' exact component={Auth(Register, false)} />
-        <Route path='/register_login' exact component={Auth(RegisterLogin, false)} />
-        <Route path='/shop' exact component={Auth(Shop, null)} />
-        <Route path='/' exact component={Auth(Home, null)} />
+        <Route path="/register" exact component={Auth(Register,false)}/>
+        <Route path="/register_login" exact component={Auth(RegisterLogin,false)}/>
+        <Route path="/shop" exact component={Auth(Shop,null)}/>
+        <Route path="/" exact component={Auth(Home,null)}/>
       </Switch>
     </Layout>
 
   )
 }
 
-export default Routes
+export default Routes;
