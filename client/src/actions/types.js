ac952090a63a7c4b1a834b560124a8a810edc14e
lopeselio <lopeselio@gmail.com>
2020-09-24 11:59:10 +0530

    types of action

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,12 +1,12 @@
-export const LOGIN_USER = 'login_user'
-export const REGISTER_USER = 'register_user'
-export const AUTH_USER = 'auth_user'
-export const LOGOUT_USER = 'logout_user'
+export const LOGIN_USER = 'login_user';
+export const REGISTER_USER = 'register_user';
+export const AUTH_USER = 'auth_user';
+export const LOGOUT_USER = 'logout_user';
 
-export const GET_PRODUCTS_BY_SELL = 'get_products_by_sell'
-export const GET_PRODUCTS_BY_ARRIVAL = 'get_products_by_arrival'
-export const GET_BRANDS = 'get_brands'
-export const GET_WOODS = 'get_woods'
-export const GET_PRODUCTS_TO_SHOP = 'get_products_to_shop'
-export const ADD_PRODUCT = 'add_product'
+export const GET_PRODUCTS_BY_SELL = 'get_products_by_sell';
+export const GET_PRODUCTS_BY_ARRIVAL = 'get_products_by_arrival';
+export const GET_BRANDS = 'get_brands';
+export const GET_WOODS =  'get_woods';
+export const GET_PRODUCTS_TO_SHOP = 'get_products_to_shop';
+export const ADD_PRODUCT = 'add_product';
 export const CLEAR_PRODUCT = 'clear_product'
