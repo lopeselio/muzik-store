d90dc88c1533371f8022d8c867893885143f6835
lopeselio <lopeselio@gmail.com>
2020-09-24 11:49:36 +0530

    user actions

--- client/src/actions/user_actions.js
+++ client/src/actions/user_actions.js
@@ -1,26 +1,26 @@
-import axios from 'axios';
+import axios from 'axios'
 import {
   LOGIN_USER,
   REGISTER_USER,
   AUTH_USER,
   LOGOUT_USER
-} from './types';
+} from './types'
 
-import { USER_SERVER } from '../components/utils/misc';
+import { USER_SERVER } from '../components/utils/misc'
 
-export function registerUser(dataToSubmit) {
+export function registerUser (dataToSubmit) {
   const request = axios.post(`${USER_SERVER}/register`, dataToSubmit)
-    .then(response => response.data);
+    .then(response => response.data)
 
-    return {
-      type: REGISTER_USER,
-      payload: request
-    }
+  return {
+    type: REGISTER_USER,
+    payload: request
+  }
 }
 
-export function loginUser(dataToSubmit) {
+export function loginUser (dataToSubmit) {
   const request = axios.post(`${USER_SERVER}/login`, dataToSubmit)
-    .then(response => response.data);
+    .then(response => response.data)
 
   return {
     type: LOGIN_USER,
@@ -28,19 +28,19 @@ export function loginUser(dataToSubmit) {
   }
 }
 
-export function auth() {
-   const request = axios.get(`${USER_SERVER}/auth`)
-   .then(response => response.data);
+export function auth () {
+  const request = axios.get(`${USER_SERVER}/auth`)
+    .then(response => response.data)
 
-   return {
-     type: AUTH_USER,
-     payload: request
-   }
+  return {
+    type: AUTH_USER,
+    payload: request
+  }
 }
 
-export function logoutUser() {
+export function logoutUser () {
   const request = axios.get(`${USER_SERVER}/logout`)
-  .then(response => response.data);
+    .then(response => response.data)
 
   return {
     type: LOGOUT_USER,
