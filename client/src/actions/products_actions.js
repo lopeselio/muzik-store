538bb915598e131191e39939ac2e19a251cbf117
lopeselio <lopeselio@gmail.com>
2020-09-24 11:49:30 +0530

    products actions

--- client/src/actions/products_actions.js
+++ client/src/actions/products_actions.js
@@ -1,100 +1,109 @@
-import axios from 'axios'
+import axios from 'axios';
 import {
-  GET_PRODUCTS_BY_SELL,
-  GET_PRODUCTS_BY_ARRIVAL,
-  GET_BRANDS,
-  GET_WOODS,
-  GET_PRODUCTS_TO_SHOP,
-  ADD_PRODUCT,
-  CLEAR_PRODUCT
-} from './types'
+    GET_PRODUCTS_BY_SELL,
+    GET_PRODUCTS_BY_ARRIVAL,
+    GET_BRANDS,
+    GET_WOODS,
+    GET_PRODUCTS_TO_SHOP,
+    ADD_PRODUCT,
+    CLEAR_PRODUCT
+} from './types';
 
-import { PRODUCT_SERVER } from '../components/utils/misc'
+import { PRODUCT_SERVER } from '../components/utils/misc';
 
 
-export function getProductsBySell () {
-  // ?sortBy=sold&order=desc&limit=100
-  const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=sold&order=desc&limit=4`)
-    .then(response => response.data)
-
-    return {
-    type: GET_PRODUCTS_BY_SELL,
-    payload: request
-  }
-}
-
-export function getProductsByArrival () {
-  const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=createdAt&order=desc&limit=4`)
-    .then(response => response.data)
+export function getProductsBySell(){
+    //?sortBy=sold&order=desc&limit=100
+    const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=sold&order=desc&limit=4`)
+                    .then(response => response.data);
 
     return {
-    type: GET_PRODUCTS_BY_ARRIVAL,
-    payload: request
-  }
+        type: GET_PRODUCTS_BY_SELL,
+        payload: request
+    }
+
 }
 
-export function getProductsToShop (skip, limit, filters = [], previousState = []) {
-  const data = {
-    limit,
-    skip,
-    filters
-  }
+export function getProductsByArrival(){
+    const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=createdAt&order=desc&limit=4`)
+    .then(response => response.data);
 
-  const request = axios.post(`${PRODUCT_SERVER}/shop`, data)
-    .then(response => {
-      const newState = [
-        ...previousState,
-        ...response.data.articles
-      ]
+    return {
+        type: GET_PRODUCTS_BY_ARRIVAL,
+        payload: request
+    }
+}
+
+export function getProductsToShop(skip, limit,filters =[], previousState = []){
+    const data = {
+        limit,
+        skip,
+        filters
+    }
+
+    const request = axios.post(`${PRODUCT_SERVER}/shop`,data)
+                .then(response => {
+                    let newState = [
+                        ...previousState,
+                        ...response.data.articles
+                    ];
                     return {
-        size: response.data.size,
-        articles: newState
-      }
-    })
+                        size: response.data.size,
+                        articles: newState
+                    }
+                });
 
     return {
-    type: GET_PRODUCTS_TO_SHOP,
-    payload: request
-  }
+        type: GET_PRODUCTS_TO_SHOP,
+        payload: request
+    }
+
 }
 
-export function addProduct (datatoSubmit) {
-  const request = axios.post(`${PRODUCT_SERVER}/article`, datatoSubmit)
-    .then(response => response.data)
+export function addProduct(datatoSubmit){
+
+    const request = axios.post(`${PRODUCT_SERVER}/article`,datatoSubmit)
+                    .then(response => response.data);
 
     return {
-    type: ADD_PRODUCT,
-    payload: request
-  }
+        type: ADD_PRODUCT,
+        payload: request
+    }
 }
 
-export function clearProduct () {
-  return {
-    type: CLEAR_PRODUCT,
-    payload: ''
-  }
+export function clearProduct(){
+    return {
+        type: CLEAR_PRODUCT,
+        payload: ''
+    }
 }
 
-/// /////////////////////////////////
-/// ///        CATEGORIES
-/// /////////////////////////////////
 
-export function getBrands () {
-  const request = axios.get(`${PRODUCT_SERVER}/brands`)
-    .then(response => response.data)
+
+
+////////////////////////////////////
+//////        CATEGORIES
+////////////////////////////////////
+
+
+export function getBrands(){
+
+    const request = axios.get(`${PRODUCT_SERVER}/brands`)
+                .then(response => response.data );
 
     return {
-    type: GET_BRANDS,
-    payload: request
-  }
+        type: GET_BRANDS,
+        payload: request
+    }
+
 }
 
-export function getWoods () {
-  const request = axios.get(`${PRODUCT_SERVER}/woods`)
-    .then(response => response.data)
+export function getWoods(){
+    const request = axios.get(`${PRODUCT_SERVER}/woods`)
+    .then(response => response.data );
 
     return {
-    type: GET_WOODS,
-    payload: request
-  }
+        type: GET_WOODS,
+        payload: request
+    }
 }
