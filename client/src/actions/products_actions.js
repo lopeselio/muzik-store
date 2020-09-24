adc1205c595efedcd29323347247e5743e44c9f5
lopeselio <lopeselio@gmail.com>
2020-09-24 09:04:14 +0530

    actions for adding and clearing product

--- client/src/actions/products_actions.js
+++ client/src/actions/products_actions.js
@@ -3,17 +3,21 @@ import {
   GET_PRODUCTS_BY_SELL,
   GET_PRODUCTS_BY_ARRIVAL,
   GET_BRANDS,
-  GET_WOODS
+  GET_WOODS,
+  GET_PRODUCTS_TO_SHOP,
+  ADD_PRODUCT,
+  CLEAR_PRODUCT
 } from './types'
 
 import { PRODUCT_SERVER } from '../components/utils/misc'
+
 
 export function getProductsBySell () {
   // ?sortBy=sold&order=desc&limit=100
   const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=sold&order=desc&limit=4`)
     .then(response => response.data)
 
-  return {
+    return {
     type: GET_PRODUCTS_BY_SELL,
     payload: request
   }
@@ -23,9 +27,51 @@ export function getProductsByArrival () {
   const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=createdAt&order=desc&limit=4`)
     .then(response => response.data)
 
-  return {
+    return {
     type: GET_PRODUCTS_BY_ARRIVAL,
     payload: request
+  }
+}
+
+export function getProductsToShop (skip, limit, filters = [], previousState = []) {
+  const data = {
+    limit,
+    skip,
+    filters
+  }
+
+  const request = axios.post(`${PRODUCT_SERVER}/shop`, data)
+    .then(response => {
+      const newState = [
+        ...previousState,
+        ...response.data.articles
+      ]
+                    return {
+        size: response.data.size,
+        articles: newState
+      }
+    })
+
+    return {
+    type: GET_PRODUCTS_TO_SHOP,
+    payload: request
+  }
+}
+
+export function addProduct (datatoSubmit) {
+  const request = axios.post(`${PRODUCT_SERVER}/article`, datatoSubmit)
+    .then(response => response.data)
+
+    return {
+    type: ADD_PRODUCT,
+    payload: request
+  }
+}
+
+export function clearProduct () {
+  return {
+    type: CLEAR_PRODUCT,
+    payload: ''
   }
 }
 
@@ -37,7 +83,7 @@ export function getBrands () {
   const request = axios.get(`${PRODUCT_SERVER}/brands`)
     .then(response => response.data)
 
-  return {
+    return {
     type: GET_BRANDS,
     payload: request
   }
@@ -47,7 +93,7 @@ export function getWoods () {
   const request = axios.get(`${PRODUCT_SERVER}/woods`)
     .then(response => response.data)
 
-  return {
+    return {
     type: GET_WOODS,
     payload: request
   }
