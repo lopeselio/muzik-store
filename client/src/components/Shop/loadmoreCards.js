31d0e299d71e35da59bb4491f086c4ca95c6ecfd
lopeselio <lopeselio@gmail.com>
2020-09-24 20:21:36 +0530

    load more cards

--- client/src/components/Shop/loadmoreCards.js
+++ client/src/components/Shop/loadmoreCards.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import CardBlockShop from '../utils/card_block_shop' 
+import React from 'react';
+import CardBlockShop from '../utils/card_block_shop'; 
 
 const LoadmoreCards = (props) => {
-  return (
-    <div>
-      <div>
-        <CardBlockShop
-          grid={props.grid}
-          list={props.products}
-        />
-      </div>
-      {
-        props.size > 0 && props.size >= props.limit
-                    ? <div className='load_more_container'>
-            <span onClick={() => props.loadMore()}>
+    return (
+        <div>
+            <div>
+                <CardBlockShop
+                    grid={props.grid}
+                    list={props.products}
+                />
+            </div>
+            {
+                props.size > 0 && props.size >= props.limit ?
+                    <div className="load_more_container">
+                        <span onClick={()=> props.loadMore()}>
                             Load More
-            </span>
-          </div>
-          : null
-      }
+                        </span>
+                    </div>
+                :null
+            }
+            
 
-
-
-    </div>
-  )
+    
+        </div>
+    );
 };
 
-export default LoadmoreCards
+export default LoadmoreCards;
