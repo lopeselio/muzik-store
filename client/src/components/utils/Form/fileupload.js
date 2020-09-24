d40c271b1fafa092ada8c87878905fbc7ccd51a1
lopeselio <lopeselio@gmail.com>
2020-09-24 12:54:04 +0530

    added dropzone for file upload

--- client/src/components/utils/Form/fileupload.js
+++ client/src/components/utils/Form/fileupload.js
@@ -1,47 +1,120 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+import axios from 'axios';
 
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import faShoppingBag from '@fortawesome/fontawesome-free-solid/faShoppingBag'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faPlusCircle from '@fortawesome/fontawesome-free-solid/faPlusCircle';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-const MyButton = (props) => {
-  const buttons = () => {
-    let template = ''
-
-    switch (props.type) {
-      case 'default':
-        template = <Link
-          className={!props.altClass ? 'link_default' : props.altClass}
-          to={props.linkTo}
-          {...props.addStyles}
-        >
-          {props.title}
-        </Link>
-        break
-      case 'bag_link':
-        template =
-          <div
-            className='bag_link'
-            onClick={() => {
-              props.runAction()
-            }}
-          >
-            <FontAwesomeIcon
-              icon={faShoppingBag}
-            />
-          </div>
-        break
-      default:
-        template = ''
+class Fileupload extends Component {
+    constructor(){
+        super();
+        this.state = {
+            uploadedFiles:[],
+            uploading:false
+        }
     }
-    return template
-  }
 
-  return (
-    <div className='my_link'>
-      {buttons()}
-    </div>
-  )
+    onDrop = (files) => {
+       this.setState({uploading:true});
+       let formData = new FormData();
+       const config = {
+           header: {'content-type':'multipart/form-data'}
+       }
+       formData.append("file",files[0]);
+
+       axios.post('/api/users/uploadimage',formData,config)
+       .then(response => {
+
+            console.log(response.data)
+
+            this.setState({
+                uploading:false,
+                uploadedFiles:[
+                    ...this.state.uploadedFiles,
+                    response.data
+                ]
+            },()=>{
+                this.props.imagesHandler(this.state.uploadedFiles)
+            })
+       });
+    }
+
+    onRemove = (id) => {
+        axios.get(`/api/users/removeimage?public_id=${id}`).then(response=>{
+            let images = this.state.uploadedFiles.filter(item=>{
+                return item.public_id !== id;
+            });
+
+            this.setState({
+                uploadedFiles: images
+            },()=>{
+                this.props.imagesHandler(images)
+            })
+        })
+    }
+
+    showUploadedImages = () => (
+        this.state.uploadedFiles.map(item=>(
+            <div className="dropzone_box"
+                key={item.public_id}
+                onClick={()=> this.onRemove(item.public_id)}
+            >
+                <div 
+                    className="wrap"
+                    style={{background:`url(${item.url}) no-repeat`}}
+                >
+                </div>
+            </div>
+        ))
+    )
+
+    static getDerivedStateFromProps(props,state){
+        if(props.reset){
+            return state = {
+                uploadedFiles:[]
+            }
+        }
+        return null;
+    }
+
+
+    render() {
+        return (
+            <div>
+                <section>
+                    <div className="dropzone clear">
+                        <Dropzone
+                            onDrop={(e)=>this.onDrop(e)}
+                            multiple={false}
+                            className="dropzone_box"
+                        >   
+                            <div className="wrap">
+                                <FontAwesomeIcon
+                                    icon={faPlusCircle}
+                                />
+                            </div>
+                        </Dropzone>
+                        { this.showUploadedImages()}
+                        {
+                            this.state.uploading ?
+                            <div className="dropzone_box" style={{
+                                textAlign: 'center',
+                                paddingTop: '60px'
+                            }}>
+                                <CircularProgress
+                                    style={{color:'#00bcd4'}}
+                                    thickness={7}
+                                />
+                            </div>
+                            :null
+                        }
+
+                    </div>
+                </section>
+            </div>
+        );
+    }
 }
 
-export default MyButton
+export default Fileupload;
