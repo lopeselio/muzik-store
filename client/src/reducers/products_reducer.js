b671ba5f85c7047d8c174b66bf847a95f0ef70c4
lopeselio <lopeselio@gmail.com>
2020-09-24 21:55:45 +0530

    reducers

--- client/src/reducers/products_reducer.js
+++ client/src/reducers/products_reducer.js
@@ -8,42 +8,42 @@ import {
   GET_PRODUCTS_TO_SHOP,
   ADD_PRODUCT,
   CLEAR_PRODUCT
-} from '../actions/types';
+} from '../actions/types'
 
 
-export default function(state={},action){
-  switch(action.type){
-      case GET_PRODUCTS_BY_SELL:
-          return {...state, bySell: action.payload }
-      case GET_PRODUCTS_BY_ARRIVAL:
-          return {...state, byArrival:  action.payload }
-      case GET_BRANDS:
-          return {...state, brands: action.payload }
-      case ADD_BRAND:
-          return {
-              ...state, 
-              addBrand: action.payload.success , 
-              brands:action.payload.brands 
-          }
-      case GET_WOODS:
-          return {...state, woods: action.payload }
-      case ADD_WOOD:
-          return {
-              ...state, 
-              addWood: action.payload.success , 
-              woods:action.payload.woods 
-          }
-      case GET_PRODUCTS_TO_SHOP:
-          return {
-              ...state,
-              toShop: action.payload.articles,
-              toShopSize: action.payload.size
-          }
-      case ADD_PRODUCT:
-          return {...state,addProduct: action.payload}
-      case CLEAR_PRODUCT:
-          return {...state,addProduct: action.payload}
-      default:
-          return state;
+export default function (state = {}, action) {
+  switch (action.type) {
+    case GET_PRODUCTS_BY_SELL:
+      return { ...state, bySell: action.payload }
+    case GET_PRODUCTS_BY_ARRIVAL:
+      return { ...state, byArrival: action.payload }
+    case GET_BRANDS:
+      return { ...state, brands: action.payload }
+    case ADD_BRAND:
+      return {
+        ...state,
+        addBrand: action.payload.success,
+        brands: action.payload.brands
+      }
+    case GET_WOODS:
+      return { ...state, woods: action.payload }
+    case ADD_WOOD:
+      return {
+        ...state,
+        addWood: action.payload.success,
+        woods: action.payload.woods
+      }
+    case GET_PRODUCTS_TO_SHOP:
+      return {
+        ...state,
+        toShop: action.payload.articles,
+        toShopSize: action.payload.size
+      }
+    case ADD_PRODUCT:
+      return { ...state, addProduct: action.payload }
+    case CLEAR_PRODUCT:
+      return { ...state, addProduct: action.payload }
+    default:
+      return state
   }
 }
