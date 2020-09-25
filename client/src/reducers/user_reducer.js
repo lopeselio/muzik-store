b362a86aca68da7a4791c7d355f0427bdacb7fb4
lopeselio <lopeselio@gmail.com>
2020-09-25 07:34:43 +0530

    cart component

--- client/src/reducers/user_reducer.js
+++ client/src/reducers/user_reducer.js
@@ -2,9 +2,11 @@ import {
   LOGIN_USER,
   REGISTER_USER,
   AUTH_USER,
-  LOGOUT_USER
+  LOGOUT_USER,
+  ADD_TO_CART_USER,
+  GET_CART_ITEMS_USER,
+  REMOVE_CART_ITEM_USER
 } from '../actions/types'
-
 
 export default function (state = {}, action) {
   switch (action.type) {
@@ -16,6 +18,25 @@ export default function (state = {}, action) {
       return { ...state, userData: action.payload }
     case LOGOUT_USER:
       return { ...state }
+    case ADD_TO_CART_USER:
+      return {
+        ...state,
+        userData: {
+          ...state.userData,
+          cart: action.payload
+        }
+      }
+    case GET_CART_ITEMS_USER:
+      return { ...state, cartDetail: action.payload }
+    case REMOVE_CART_ITEM_USER:
+      return {
+        ...state,
+        cartDetail: action.payload.cartDetail,
+        userData: {
+          ...state.userData,
+          cart: action.payload.cart
+        }
+      }
     default:
       return state
   }
