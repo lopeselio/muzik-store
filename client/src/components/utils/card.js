af31779911553bc2c5efef296557c8887d9d83c5
lopeselio <lopeselio@gmail.com>
2020-09-25 07:35:01 +0530

    product block cart

--- client/src/components/utils/card.js
+++ client/src/components/utils/card.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import MyButton from './button';
 
+import { connect } from 'react-redux';
+import { addToCart } from '../../actions/user_actions';
+
 class Card extends Component {
 
     renderCardImage(images){
@@ -53,7 +56,10 @@ class Card extends Component {
                             <MyButton
                                 type="bag_link"
                                 runAction={()=>{
-                                    console.log('added to cart')
+                                    props.user.userData.isAuth ?
+                                        this.props.dispatch(addToCart(props._id))
+                                    :
+                                        console.log('you need to log in')
                                 }}
                             />
                         </div>
@@ -64,4 +70,10 @@ class Card extends Component {
     }
 }
 
-export default Card;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps)(Card);
