b6d55743b11913674b502a4ef4e51ba0b041aaf1
lopeselio <lopeselio@gmail.com>
2020-09-25 06:09:17 +0530

    added routes to cart

--- client/src/routes.js
+++ client/src/routes.js
@@ -8,6 +8,7 @@ import Home from './components/Home';
 import RegisterLogin from './components/Register_login';
 import Register from './components/Register_login/register';
 import Shop from './components/Shop';
+import ProductPage from './components/Product';
 
 import UserDashboard from './components/User';
 import AddProduct from './components/User/Admin/add_product';
@@ -21,6 +22,7 @@ const Routes = () => {
         <Route path="/admin/add_product" exact component={Auth(AddProduct,true)}/>
         <Route path="/admin/manage_categories" exact component={Auth(ManageCategories,true)}/>
 
+        <Route path="/product_detail/:id" exact component={Auth(ProductPage,null)}/>
         <Route path="/register" exact component={Auth(Register,false)}/>
         <Route path="/register_login" exact component={Auth(RegisterLogin,false)}/>
         <Route path="/shop" exact component={Auth(Shop,null)}/>
