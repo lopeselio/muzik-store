5ff42d875fca29511be66426d1a803045fc808f3
lopeselio <lopeselio@gmail.com>
2020-09-25 06:09:36 +0530

    product type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,6 +2,7 @@ export const LOGIN_USER = 'login_user';
 export const REGISTER_USER = 'register_user';
 export const AUTH_USER = 'auth_user';
 export const LOGOUT_USER = 'logout_user';
+export const ADD_TO_CART_USER = 'add_to_cart_user';
 
 export const GET_PRODUCTS_BY_SELL = 'get_products_by_sell';
 export const GET_PRODUCTS_BY_ARRIVAL = 'get_products_by_arrival';
@@ -11,4 +12,6 @@ export const GET_WOODS =  'get_woods';
 export const ADD_WOOD = 'add_wood';
 export const GET_PRODUCTS_TO_SHOP = 'get_products_to_shop';
 export const ADD_PRODUCT = 'add_product';
-export const CLEAR_PRODUCT = 'clear_product'
+export const CLEAR_PRODUCT = 'clear_product';
+export const GET_PRODUCT_DETAIL = 'get_product_detail';
+export const CLEAR_PRODUCT_DETAIL = 'clear_product_detail';
