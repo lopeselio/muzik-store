e1a3f086c62724e1c6a655ef22a500e98598936e
lopeselio <lopeselio@gmail.com>
2020-10-18 21:57:24 +0530

    site reducer

---
+++ client/src/reducers/site_reducer.js
@@ -0,0 +1,15 @@
+import {
+  GET_SITE_DATA,
+  UPDATE_SITE_DATA
+} from '../actions/types';
+
+export default function(state={},action){
+  switch(action.type){
+      case GET_SITE_DATA:
+          return { ...state, siteData: action.payload }
+      case UPDATE_SITE_DATA:
+          return { ...state, siteData: action.payload.siteInfo }
+      default:
+          return state;
+  }
+}
