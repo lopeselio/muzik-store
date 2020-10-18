61a724253d1ec257fa1bb0d54faeeb4cf28b0920
lopeselio <lopeselio@gmail.com>
2020-10-18 21:59:16 +0530

    managing site info

---
+++ client/src/components/User/Admin/manage_site.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserLayout from '../../../hoc/user';
+import UpdateSiteNfo from './update_site_nfo';
+
+const ManageSite = () => {
+    return (
+        <UserLayout>
+           <UpdateSiteNfo/>
+        </UserLayout>
+    );
+};
+
+export default ManageSite;
