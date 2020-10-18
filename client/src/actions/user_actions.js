df36d8920b52a91fa66f926719d16c1e013fd66f
lopeselio <lopeselio@gmail.com>
2020-10-18 21:58:46 +0530

    user actions

--- client/src/actions/user_actions.js
+++ client/src/actions/user_actions.js
@@ -6,7 +6,10 @@ import {
     LOGOUT_USER,
     ADD_TO_CART_USER,
     GET_CART_ITEMS_USER,
-    REMOVE_CART_ITEM_USER
+    REMOVE_CART_ITEM_USER,
+    ON_SUCCESS_BUY_USER,
+    UPDATE_DATA_USER,
+    CLEAR_UPDATE_USER_DATA
 } from './types';
 
 import { USER_SERVER, PRODUCT_SERVER } from '../components/utils/misc';
@@ -113,4 +116,35 @@ export function removeCartItem(id){
         payload: request
     }
 
+}
+
+
+
+export function onSuccessBuy(data){ 
+    const request = axios.post(`${USER_SERVER}/successBuy`,data)
+                    .then(response => response.data);
+
+    return {
+        type: ON_SUCCESS_BUY_USER,
+        payload: request
+    }
+}
+
+export function updateUserData(dataToSubmit){
+    const request = axios.post(`${USER_SERVER}/update_profile`,dataToSubmit)
+                    .then(response => {
+                        return response.data
+                    });
+    
+    return {
+        type: UPDATE_DATA_USER,
+        payload: request
+    }
+}
+
+export function clearUpdateUser(){
+    return {
+        type: CLEAR_UPDATE_USER_DATA,
+        payload: ''
+    }
 }
