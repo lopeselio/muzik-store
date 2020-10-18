78ce85a4cf37c2e23e31eb267b73b2934a7f959a
lopeselio <lopeselio@gmail.com>
2020-10-18 21:58:40 +0530

    types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -5,6 +5,9 @@ export const LOGOUT_USER = 'logout_user';
 export const ADD_TO_CART_USER = 'add_to_cart_user';
 export const GET_CART_ITEMS_USER = 'get_cart_item_user';
 export const REMOVE_CART_ITEM_USER = 'remove_cart_item_user';
+export const ON_SUCCESS_BUY_USER = 'on_success_buy_user';
+export const UPDATE_DATA_USER = 'update_data_user';
+export const CLEAR_UPDATE_USER_DATA = 'clear_update_user_data'
 
 export const GET_PRODUCTS_BY_SELL = 'get_products_by_sell';
 export const GET_PRODUCTS_BY_ARRIVAL = 'get_products_by_arrival';
@@ -18,3 +21,5 @@ export const CLEAR_PRODUCT = 'clear_product';
 export const GET_PRODUCT_DETAIL = 'get_product_detail';
 export const CLEAR_PRODUCT_DETAIL = 'clear_product_detail';
 
+export const GET_SITE_DATA = 'get_site_data';
+export const UPDATE_SITE_DATA = 'update_site_data';
