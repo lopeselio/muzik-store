5f728584fc6f27d8fedfa54a26a4822b91187353
Cry07 <lopesdevelioper@gmail.com>
2020-10-22 10:34:23 +0530

    footer states

--- client/src/components/Header_Footer/Footer/index.js
+++ client/src/components/Header_Footer/Footer/index.js
@@ -6,12 +6,14 @@ import faClock from '@fortawesome/fontawesome-free-solid/faClock';
 import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
 
 
-const Footer = () => {
+
+const Footer = ({data}) => {
     return (
+        data.siteData ?
         <footer className="bck_b_dark">
             <div className="container">
                 <div className="logo">
-                    MUZIK!
+                    Waves
                 </div>
                 <div className="wrapper">
                     <div className="left">
@@ -24,7 +26,7 @@ const Footer = () => {
                                 />
                                 <div className="nfo">
                                     <div>Address</div>
-                                    <div>Kramer 2345</div>
+                                    <div>{data.siteData[0].address}</div>
                                 </div>
                             </div>
                             <div className="tag">
@@ -34,7 +36,7 @@ const Footer = () => {
                                 />
                                 <div className="nfo">
                                     <div>Phone</div>
-                                    <div>2345-22222</div>
+                                    <div>{data.siteData[0].phone}</div>
                                 </div>
                             </div>
                             <div className="tag">
@@ -44,7 +46,7 @@ const Footer = () => {
                                 />
                                 <div className="nfo">
                                     <div>Working hours</div>
-                                    <div>Mon-Sun/ 9am-8pm</div>
+                                    <div>{data.siteData[0].hours}</div>
                                 </div>
                             </div>
                             <div className="tag">
@@ -54,7 +56,7 @@ const Footer = () => {
                                 />
                                 <div className="nfo">
                                     <div>Email</div>
-                                    <div>nfo@waves.com</div>
+                                    <div>{data.siteData[0].email}</div>
                                 </div>
                             </div>
                         </div>
@@ -70,6 +72,7 @@ const Footer = () => {
                 </div>
             </div>
         </footer>
+        :null
     );
 };
 
